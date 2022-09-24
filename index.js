1ac64e0a29ba7bb29fba3f0be9c79b679ee5f365
Hansel <you@example.com>
2022-09-24 15:34:44 -0400

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+/* backgorund img loop */
+
+function tile(url, left, bottom, width, height){
+    for(let i = 0; i < height; i++){
+        for(let j = 0; j < width; j++){
+            newImage(url, left + j*100, bottom+i*100)
+        }
+    }
+}
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+
+
+/* Images Structure */
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+/*item Structure*/
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+    object.addEventListener('dblclick', function(){
+        object.remove()
+    })}
+
+   /*images src*/
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+newImage('assets/green-character.gif',100 ,100)
+
+
+/*items src*/
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
